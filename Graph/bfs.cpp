dc483a2341b11ecddee5452e7b24958ee45ea7b3
TheBugg <138120876+deepgodhani@users.noreply.github.com>
2024-12-09 21:48:23 +0530

    bfs.cpp

--- Graph/bfs.cpp
+++ Graph/bfs.cpp
@@ -1,1 +1,26 @@
+void bfs(int source,vector<vector<int>>& adj,int n){
 
+	vector<bool> visited(n,false);
+	queue<int> q;
+	q.push(source);
+	visited[source]=true;
+	int level=0;
+	while(!q.empty()){
+		int s=q.size();
+		while(s--){
+			int node=q.front();
+			q.pop();
+				cout<<" "<<node<< " ";
+			for(auto neighbour:adj[node]){
+					if(!visited[neighbour]){
+						q.push(neighbour);
+						visited[neighbour]=true;
+					}
+			}
+
+		}
+		level++;
+	}
+
+	//return ;
+}
